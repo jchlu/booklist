58514cfb6a7dc4a3e61ef4e1df8a6736e31805ca
Johnny Choudhury-Lucas <johnny@exito.tech>
2019-05-23 23:09:32 -0400

    Init and connect Redux

---
+++ src/reducers/books.js
@@ -0,0 +1,7 @@
+export default () => [
+  { title: 'Eloquent Ruby' },
+  { title: 'The Secret Life of Plants' },
+  { title: 'From Russia with Love' },
+  { title: 'Smiley\'s People' },
+  { title: 'Bridge of Spies' },
+]
