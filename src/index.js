aeed056eba7c7fb0b500ad415e23c11fc56d3155
Johnny Choudhury-Lucas <johnny@exito.tech>
2019-05-24 12:34:39 -0400

    Change entry point to an id

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
-import App from './components/app';
-import reducers from './reducers';
+import App from './components/app'
+import reducers from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 // const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -16,4 +16,4 @@ ReactDOM.render(
       ))}>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.getElementById('app'))
